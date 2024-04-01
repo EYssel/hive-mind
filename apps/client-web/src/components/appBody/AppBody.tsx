50ee423774d0fc7096d1670b435ced1a0c8e40a6
Estian Yssel <estianyssel@gmail.com>
2024-04-01 16:45:58 +0200

    refact(client-web): cleanup component

--- apps/client-web/src/components/appBody/AppBody.tsx
+++ apps/client-web/src/components/appBody/AppBody.tsx
@@ -1,15 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { HomeRoute, PostsRoute } from "..";
-import { PostRoute } from "../posts/PostRoute";
+import { PostRoute } from "../posts";
 
 export function AppBody() {
     return (
-        <>
-            <Routes>
-                <Route path="/" element={<HomeRoute />} />
-                <Route path="/posts" element={<PostsRoute />} />{" "}
-                <Route path="/post/:id" element={<PostRoute />} />
-            </Routes>
-        </>
+        <Routes>
+            <Route path="/" element={<HomeRoute />} />
+            <Route path="/posts" element={<PostsRoute />} />{" "}
+            <Route path="/post/:id" element={<PostRoute />} />
+        </Routes>
     );
 }
